f615a73da9e907bce3a1bd1d7f61f3867d4d47b4
mominmalik11 <misformomin@gmail.com>
2024-01-22 21:59:17 -0500

    pushing another tset case Add2Cart

---
+++ src/test/java/com/tutorialsninja/TestCases/AddToProductTest.java
@@ -0,0 +1,58 @@
+package com.tutorialsninja.TestCases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class AddToProductTest {
+	
+	
+	public WebDriver driver;
+	public ChromeOptions options;
+	
+	
+	@BeforeMethod
+	public void loginSetup() {
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://tutorialsninja.com/demo/");		
+	}
+	
+	@Test
+	public void testCase1() throws Exception {
+		driver.findElement(By.cssSelector("#search > input")).sendKeys("HP");
+		driver.findElement(By.cssSelector("#search > span > button")).click();
+		driver.findElement(By.xpath("//*[@id=\"content\"]/div[3]/div/div/div[2]/div[2]/button[1]")).click();
+		driver.findElement(By.xpath("//*[@id=\"cart\"]/button")).click();
+		//Assert.assertTrue(driver.findElement(By.xpath("//li[contains(text(), 'Product Code:Product 21')]")).isDisplayed());
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//button[@id = 'button-cart']")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//div[@id = 'cart']")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.linkText("Checkout")).click();
+		
+	
+
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	@AfterMethod
+	public void tearDown() {
+		
+		driver.quit();
+
+}
+
+}
